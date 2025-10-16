20fe329996dd1342d193cc32ac4b2c41fc244616
Matheus Ribeiro <matheusribeirofon20@gmail.com>
2025-10-15 21:29:07 -0300

    Criando programa de variáveis

---
+++ world_1/02_variaveis.py
@@ -0,0 +1,22 @@
+'''
+O que é uma variável?
+Valor de referência na memoria que você vai gravar na execução do programa
+'''
+
+#Definindo variáveis
+name = "Resident Evil 4 Remake"
+yearLaunch = 2023
+gamePrice = 300.00
+planInclude = True
+
+#Imprimindo valores das minhas variáveis
+print(name)
+print(yearLaunch)
+print(gamePrice)
+print(planInclude)
+
+#Estou imprimindo o tipo de dados das minhas variáveis, graças a função type
+print(type(name))
+print(type(yearLaunch))
+print(type(gamePrice))
+print(type(planInclude))
